f03c7f8cc68bf0efa87074f076eb7fbe129fc106
Omar Faruk <omarfaruk.dev@gmail.com>
2025-05-25 21:29:48 +0600

    initial file and route setup

---
+++ src/Routes/router.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: MainLayout,
+        children: [
+            {
+                path: '/',
+                Component: Home,
+            },
+            {
+                path: '/contact',
+                element: <h2>Contact Page</h2>            
+            },
+            {
+                path: '*',
+                Component: ErrorPage
+            }
+        ]
+    }
+])
+
+
+export default router;
